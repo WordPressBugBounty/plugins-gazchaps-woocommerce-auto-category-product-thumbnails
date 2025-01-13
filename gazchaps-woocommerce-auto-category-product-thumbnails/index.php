0557d4b0ffedf998b264111d0b169531f6f0a634
Sean Murphy <sean@iamseanmurphy.com>
2025-01-13 07:20:40 +0000

    1.5

--- gazchaps-woocommerce-auto-category-product-thumbnails/index.php
+++ gazchaps-woocommerce-auto-category-product-thumbnails/index.php
@@ -6,9 +6,9 @@
  * Author: Gareth 'GazChap' Griffiths
  * Author URI: https://www.gazchap.com/
  * Description: Automatically use a product thumbnail as a category thumbnail if no category thumbnail is set
- * Tested up to: 6.6.1
+ * Tested up to: 6.7.1
  * WC requires at least: 3.0.0
- * WC tested up to: 9.1.0
+ * WC tested up to: 9.5.2
  * Text Domain: gazchaps-woocommerce-auto-category-product-thumbnails
  * Domain Path: /lang
  * License: GNU General Public License v2.0
--- gazchaps-woocommerce-auto-category-product-thumbnails/readme.txt
+++ gazchaps-woocommerce-auto-category-product-thumbnails/readme.txt
@@ -3,9 +3,9 @@ Contributors: gazchap
 Tags: woocommerce,ecommerce,images,thumbnails
 Requires at least: 4.5.0
 Requires PHP: 5.6
-Tested up to: 6.6.1
+Tested up to: 6.7.1
 WC requires at least: 3.0.0
-WC tested up to: 9.1.0
+WC tested up to: 9.5.2
 License: GNU General Public License v2.0
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
